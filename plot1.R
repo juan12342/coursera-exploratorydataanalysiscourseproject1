fe4adead74fcb158a6777351f3eda81cf2f0b6d5
juan <51038935+juan12342@users.noreply.github.com>
2020-08-23 19:38:58 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(lubridate)
+
+# reading relevant data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clearing name row and unrelevant variables, creating numerics
+data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+
+# create histogram
+png("plot1.png", width=480, height=480)
+hist(data$V3, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+dev.off()
